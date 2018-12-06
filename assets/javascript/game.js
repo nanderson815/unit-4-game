749f0b792f56a02ff0e123cef7c95ad3a9fb3c63
Noah A <nanderson815@gmail.com>
2018-12-06 17:10:54 -0500

    Updated 12 6 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+
+    // Array of crystal images.
+    var crystalImages = [
+        "assets/images/bluecrystal.png",
+        "assets/images/greenCrystal.png",
+        "assets/images/orangeCrystal.png",
+        "assets/images/redcrystal.png"
+    ];
+
+
     // Generates a random number and prints it to the DOM.
     var randomNumber = Math.floor(Math.random()*100);
     $(".numberDiv").text(randomNumber);
 
-
+    for ( i=0; i < crystalImages.length; i++){
+        var crystal = $("<img>");
+        crystal.attr("src", crystalImages[i]);
+        $(".crystalsDiv").append(crystal);
+    }
 
 });
