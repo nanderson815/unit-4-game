1227f77fcb4f533cc3067cbbcfaf6d7625f8f3fd
Noah A <nanderson815@gmail.com>
2018-12-06 18:27:11 -0500

    Updated 12 6 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
     ];
 
 
-    // Generates a random number and prints it to the DOM.
-    var randomNumber = Math.floor(Math.random() * 100);
+    // Generates a random number between 50 and 100 and prints it to the DOM.
+    var randomNumber = Math.floor(Math.random() * 50) + 50;
     $(".numberDiv").text(randomNumber);
 
     // Grabs an image for the crystalImages array and prints it to the DOM
@@ -18,6 +18,10 @@ $(document).ready(function () {
         var crystal = $("<img>");
         crystal.attr("src", crystalImages[i]);
         $(".crystalsDiv").append(crystal);
+
+        // gets a random number between 1 and 25 for each crystal.
+        var crystalValue = Math.floor(Math.random()*24)+1;
+        crystal.attr("crystalValue", crystalValue);
     }
 
 });
