32d4aadbe7b98aad5a87a49779a9c2a86873095c
Noah A <nanderson815@gmail.com>
2018-12-07 09:30:21 -0500

    Updated 12 8 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,20 +8,63 @@ $(document).ready(function () {
         "assets/images/redcrystal.png"
     ];
 
+    var gameScore = 0;
+    var randomNumber = 0;
+    var wins = 0;
+    var losses = 0;
+
+
 
     // Generates a random number between 50 and 100 and prints it to the DOM.
-    var randomNumber = Math.floor(Math.random() * 50) + 50;
-    $(".numberDiv").text(randomNumber);
+    function generateRandomNumber() {
+        randomNumber = Math.floor(Math.random() * 50) + 50;
+        $(".numberDiv").text(randomNumber);
+    }
+    generateRandomNumber();
 
     // Grabs an image for the crystalImages array and prints it to the DOM
     for (i = 0; i < crystalImages.length; i++) {
         var crystal = $("<img>");
         crystal.attr("src", crystalImages[i]);
+        crystal.attr("class", "gameCrystal");
         $(".crystalsDiv").append(crystal);
 
-        // gets a random number between 1 and 25 for each crystal.
-        var crystalValue = Math.floor(Math.random()*24)+1;
-        crystal.attr("crystalValue", crystalValue);
+        // gets a random number between 1 and 15 for each crystal.
+        function generateCrystalNumber() {
+            var crystalValue = Math.floor(Math.random() * 14) + 1;
+            crystal.attr("crystalValue", crystalValue);
+        }
+        generateCrystalNumber();
     }
+
+    $(".gameCrystal").on("click", function () {
+
+        if (gameScore < randomNumber) {
+            var crystalScore = $(this).attr('crystalValue');
+            gameScore = gameScore + parseInt(crystalScore);
+            $(".currentScoreDiv").text(gameScore);
+        }
+
+        if (gameScore === randomNumber) {
+            alert("You Win!")
+            wins++;
+            $(".wins").text(wins);
+            gameScore = 0;
+            $(".currentScoreDiv").text(gameScore);
+            generateCrystalNumber();
+            generateRandomNumber();
+
+        } else if (gameScore > randomNumber) {
+            alert("You Lose!");
+            losses++;
+            $(".losses").text(losses);
+            gameScore = 0;
+            $(".currentScoreDiv").text(gameScore);
+            generateCrystalNumber();
+            generateRandomNumber();
+        }
+    });
+
+
 
 });
